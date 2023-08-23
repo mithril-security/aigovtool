c4e13a2c3d2efe97429e90bf0cdf7a328f5c8094
Yass <bargach.y@gmail.com>
2023-08-23 11:53:44 +0000

    install scripts

---
+++ install_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Changing default to nightly
+rustup default nightly
+rustup target add x86_64-fortanix-unknown-sgx --toolchain nightly
+source "$HOME/.cargo/env"
+
+# Fortanix & Just
+cargo install fortanix-sgx-tools ftxsgx-simulator sgxs-tools --git https://github.com/mithril-security/rust-sgx --branch sim-mode
+
+export BLINDAI_AZURE_DCSV3_PATCH=1
+export SGX_AESM_ADDR=1
---
+++ install_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get install -y libcurl4 libssl1.1 make cmake jq pkg-config libssl-dev protobuf-compiler curl gnupg software-properties-common
+
+# Intel SGX 
+echo "deb https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -cs) main" | tee -a /etc/apt/sources.list.d/intel-sgx.list >/dev/null 
+
+curl -sSL "https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key" | apt-key add - 
+apt-get update
+apt-get install -y sgx-aesm-service libsgx-aesm-launch-plugin
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/bin
+apt-get remove -y libsgx-dcap-default-qpl 
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+sudo add-apt-repository "https://packages.microsoft.com/ubuntu/20.04/prod"
+apt-get update && apt-get install -y az-dcap-client
+ln -s /usr/lib/libdcap_quoteprov.so /usr/lib/x86_64-linux-gnu/libdcap_quoteprov.so.1
