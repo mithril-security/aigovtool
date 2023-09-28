d58b3b979e2ddae34ac217bde2784cc49a00559d
Ubuntu <azureuser@fli-corentin-test.twmaw0abkutuhgqss0yfuzlg3g.ax.internal.cloudapp.net>
2023-09-28 08:43:33 +0000

    Modification of yesterday

--- install_config.sh
+++ install_config.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+source "$HOME/.cargo/env"
+
 # Changing default to nightly
 rustup default nightly
 rustup target add x86_64-fortanix-unknown-sgx --toolchain nightly
+
 source "$HOME/.cargo/env"
 
 # Fortanix & Just
